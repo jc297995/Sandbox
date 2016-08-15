eea5fb5d6b7acd245f0e1af9af94bbf1887887f1
MiguelVerroya <miguelgia.verroya@my.jcu.edu.au>
2016-08-15 14:37:32 +1000

    Implement the oddName program

--- oddName.py
+++ oddName.py
@@ -1,5 +1,12 @@
 """
 
 Miguel Verroya
-
 """
+name = input("Please Enter Name")
+
+while name == "":
+    name = input("Please Enter Name")
+
+for i in range(0, len(name), 2):
+    print(name[i], end='')
+
